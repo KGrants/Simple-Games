07768ebd5b009069193f4ce133eb04350caa9f11
KGrants <118312276+KGrants@users.noreply.github.com>
2022-12-19 00:44:46 +0200

    19-DEC-2022 changes
    Added Split option, but still has multiple bugs (wager doubled for second player hand if first was a double).
    And currently split is only available one time, this needs to be changed.

---
+++ Blackjack/Actions.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Threading.Tasks.Dataflow;
+
+namespace blackjack
+{ 
+	public class Actions
+	{	
+        ///Used to add cards to the table
+		public static string takecard()
+        {
+            string[] cards = new string[] {"2","3","4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"};
+            var rndMember = cards[RandomNumberGenerator.GetInt32(cards.Length)];
+            return rndMember;
+        }
+
+        ///Used to calculate results when game is over (if player ran out of money or pressed Q)
+        public static void endgame(int bank, int startbank)
+        {
+            Console.WriteLine("Thank you for playing.");
+                if (bank > startbank)
+                    Console.WriteLine($"You have won {bank-startbank} dollars and walk away with {bank} dollars!");
+                else if (bank == startbank)
+                    Console.WriteLine($"You have the same balance as you started with - {bank}!");
+                else
+                    Console.WriteLine($"You have lost {startbank-bank} dollars and walk away with {bank} dollars");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+
+        ///Used to display the status of the game (not for Split)
+        public static void showcards(List<string> userhand, List<string> dealerhand, int userpoints, int dealerpoints)
+        {
+            Console.WriteLine($"You    :  {String.Join(" ", userhand)}  ({userpoints})");
+            Console.WriteLine($"Dealer :  {String.Join(" ", dealerhand)}  ({dealerpoints})");
+        }
+
+        ///Used to calculate score based on player or dealer cards
+        public static int calculatescore(List<string> a)
+        {
+            int[] scores = new int[a.Count];
+            for (int i = 0; i < a.Count(); i++)
+            {
+                if (!int.TryParse(a[i], out int r))
+                {
+                    if (a[i] == "A")
+                        scores[i] = 11;
+                    else
+                        scores[i] = 10;
+                }
+                else
+                    scores[i] = r;
+            }
+            while (scores.Sum() > 21 && Array.IndexOf(scores,11)!=-1)
+                scores[Array.IndexOf(scores,11)] = 1;
+
+            return scores.Sum();
+        }
+
+        public static int calculatescore(string a)
+        {
+            if (!int.TryParse(a, out int r))
+                {
+                    if (a == "A")
+                        return 11;
+                    else
+                        return 10;
+                }
+            else
+                return r;
+        }
+
+        public static int getresults(int userpoints, int dealerpoints, int wager, int bank)
+        {
+            if (userpoints > dealerpoints) 
+                Console.WriteLine($"Congratulations! You won {wager} dollars, current bank balance {bank + wager}");
+
+            else if (userpoints == dealerpoints)
+            { 
+                Console.WriteLine($"It's a tie, current bank balance {bank}");
+                wager = 0;
+            }
+            else
+            {
+                Console.WriteLine($"Too bad! You lost {wager}, current bank balance {bank-wager}");
+                wager = -wager;
+            }
+            return wager;
+        }
+
+        public static int getbudget()
+        {  
+            while (true)
+            {
+                Console.WriteLine("How much did you wife allow you to spend on this game?");
+                var input = Console.ReadLine();
+                if (!int.TryParse(input, out int r))
+                {
+                    Console.WriteLine("Please add an integer value to continue;");
+                    continue;
+                }
+                else
+                {
+                    if (r > 0) 
+                        return r;
+
+                    else
+                    {
+                        Console.WriteLine("We are not handing out any loans here! Please add a valid budget!");
+                        continue;
+                    }    
+                }
+            }
+        }
+
+        public static string getandvalidateinput()
+        {
+            while(true)
+            { 
+                Console.WriteLine("Hit (H), Stand (S), Double(D) or Split(X)?");
+                string? input = Console.ReadLine();
+
+                if (input!="H" && input!="S" && input!="D" && input!="X")
+                {
+                    Console.WriteLine("Please add a valid input!");
+                    continue;
+                }
+                else
+                    return input;
+            }
+        }
+
+        public static string getandvalidateinputaftersplit()
+        {
+            while(true)
+            { 
+                Console.WriteLine("Hit (H), Stand (S), or Double(D)?");
+                string? input = Console.ReadLine();
+
+                if (input!="H" && input!="S" && input!="D")
+                {
+                    Console.WriteLine("Please add a valid input!");
+                    continue;
+                }
+                else
+                    return input;
+            }
+        }
+	}
+}
+
--- Blackjack/Program.cs
+++ Blackjack/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Security.Cryptography;
 using System.Threading.Tasks.Dataflow;
+using static blackjack.Actions;
 
 namespace blackjack
 {
@@ -8,47 +9,55 @@ namespace blackjack
     {
         static void Main(string[] args)
         {
-            int bank = 100;
 
             Console.WriteLine("Let's play a little game of Blackjack.");
-            Console.WriteLine("Your wife has noted that you only have 100 dollars available for this evening.");
-            Console.WriteLine("To play the game of Blackjack, you have to bet at least 1 dollar.");
-            Console.WriteLine("You can quit game at any point by adding Q to console when it asks for your bet");
-            Console.WriteLine("\nReady to start?");
-            Console.ReadKey();
-        
+
+            ///Next line gets and validates budget from player.
+            int bank = getbudget();  
+            /// Will be used for calculating end results.
+            int startbank = bank;
+                            
+            Console.WriteLine($"Your budget is {bank} dollars.");
+            Console.WriteLine("To play a round of Blackjack, you have to bet at least 1 dollar.");
+            Console.WriteLine("You can quit game at any point by adding Q to console when it asks for your bet.");
+
+            /// Here starts the game
             while (bank>0)
             {
-                List<string> userhand = new List<string>();
-                List<string> dealerhand = new List<string>();
-                int userpoints = 0, dealerpoints = 0, wager = 0;
-
-                Console.Clear();
-                Console.WriteLine("\nPlace your bet!");
+                Console.WriteLine("\nPlace your bet! (Q to quit)");
                 Console.WriteLine($"Bank balance = {bank}");
-                string bet = Console.ReadLine();
-                
-                if (bet == "Q")
-                    endgame(bank);
 
-                if (!int.TryParse(bet, out int r)||int.Parse(bet)<0 ||int.Parse(bet)>bank)
+                List<string> userhand = new List<string>();
+                List<List<string>> alluserhands = new List<List<string>>();
+                List<string> dealerhand = new List<string>();
+                int wager = 0;
+                string? bet = Console.ReadLine();
+                Console.Clear();
+                
+                /// if Q, quit game,calculate and show results.
+                if (bet == "Q")
+                    endgame(bank, startbank);
+
+                /// if input is not valid jump to next iteration of while loop
+                if (!int.TryParse(bet, out int rr)||int.Parse(bet)<=0 ||int.Parse(bet)>bank)
                 {
-                    Console.WriteLine("Please add a valid bet or add Q to end game!");
+                    Console.WriteLine("Please add a valid bet or Q to quit the table!");
                     continue;
                 }
 
-                wager = int.Parse(bet);
+                /// assign bet to wager to use as int
+                wager = rr;
 
-                userhand.Add(takecard().Item1);
-                userhand.Add(takecard().Item1);
-                userpoints = calculatescore(userhand);
+                /// This part get initial cards for player and dealer, calculates scores and shows both.
+                userhand.Add(takecard());
+                userhand.Add(takecard());
+                dealerhand.Add(takecard());
 
-                dealerhand.Add(takecard().Item1);
-                dealerpoints = calculatescore(dealerhand);
+                Console.WriteLine($"Your bet = {wager}, bank balance = {bank}");
+                showcards(userhand,dealerhand,calculatescore(userhand),calculatescore(dealerhand));
 
-                showcards(userhand,dealerhand,userpoints,dealerpoints);
-
-                if (userpoints ==21)
+                /// If player has blackjack, this round is over, dealer pays 1,5 times bet and goes to next while iteration
+                if (calculatescore(userhand) == 21)
                 {
                     wager = (int)(Math.Ceiling(wager*1.5));
                     bank += wager;
@@ -56,136 +65,157 @@ namespace blackjack
                     continue;
                 }
 
+                /// If there is no blackjack, we need further input from player
                 while(true)
                 {
-                    Console.WriteLine("Hit (H), Stay (S) or Double(D)?");
-                    string input = Console.ReadLine();
+                    /// Next line asks for player action, validates it and assigns to input
+                    string input = getandvalidateinput();
 
-                    if (input!="H" && input!="S" && input!="D")
-                    {
-                        Console.WriteLine("Please add a valid input!");
-                        continue;
+                    /// If player wants to "STAY", we break loop and continue with dealer turns and results.
+                    if (input == "S")
+                    { 
+                        alluserhands.Add(userhand);
+                        break;    
                     }
+
+
+                    /// If "HIT" then we add another card on the table, calculate scores and show latest situation
                     else if (input=="H")
                     {
-                        userhand.Add(takecard().Item1);
-                        userpoints = calculatescore(userhand);
-                        showcards(userhand,dealerhand,userpoints,dealerpoints);
+                        userhand.Add(takecard());
+                        showcards(userhand,dealerhand,calculatescore(userhand),calculatescore(dealerhand));
 
-                        if (userpoints>21)
+                        /// If userpoints goes over 21 then it's a bust - round is over for player.
+                        if (calculatescore(userhand)>21)
+                        { 
+                            alluserhands.Add(userhand);
                             break;
+                        }
                     }
+
+                    /// If player wants to "DOUBLE" we double wager, add one card and break loop.
                     else if (input=="D")
-                    {
+                    {   
+                        ///Validates if user has enough balance for this action
                         if (wager*2>bank)
                         {
                             Console.WriteLine("Insufficient bank balance");
                             continue;
                         }
                         wager *=2;
-                        userhand.Add(takecard().Item1);
-                        userpoints = calculatescore(userhand);
-                        showcards(userhand,dealerhand,userpoints,dealerpoints);
+                        userhand.Add(takecard());
+                        showcards(userhand,dealerhand,calculatescore(userhand),calculatescore(dealerhand));
+                        alluserhands.Add(userhand);
+                        break;
+                        
+                    }
+
+                    /// if player wants to "SPLIT" -- this part I'm still working on
+                    else if (input=="X")
+                    {
+                        ///Validates if user has enough balance for this action 
+                        if (wager*2>bank)
+                        {
+                            Console.WriteLine("Insufficient bank balance");
+                            continue;
+                        }
+                        ///Validates if both cards are of the same value.
+                        if (calculatescore(userhand[0]) != calculatescore(userhand[1]))
+                        {
+                            Console.WriteLine("You can only split your hand if your initial two-card hand includes two cards of the same value!");
+                            continue;
+                        }
+
+                        /// This is the moment we split both cards into two lists
+                        List<string> userhand2 = new List<string>() {userhand.ElementAt(1)};
+                        userhand.Remove(userhand.ElementAt(1));
+
+                        Console.Clear();
+
+                        Console.WriteLine($"Your bet is {wager} for each hand ({wager*2} in total), bank balance {bank}");
+                        Console.WriteLine($"First hand    :  {String.Join(" ", userhand)}  ({calculatescore(userhand)})");
+                        Console.WriteLine($"Second hand   :  {String.Join(" ", userhand2)}  ({calculatescore(userhand2)})");
+
+                        /// Creating list of lists to use foreach after
+                        List<List<string>> bothhands = new List<List<string>>() {userhand,userhand2};
+
+                        foreach(List<string> hand in bothhands)
+                        {
+                            while(true)
+                            {
+                                Console.WriteLine($"\nHand at play: {String.Join(" ", hand)}  ({calculatescore(hand)})");
+                                input = getandvalidateinputaftersplit();
+
+                                if (input == "S")
+                                    break;
+
+                                else if (input=="H")
+                                {
+                                    hand.Add(takecard());
+                                    showcards(hand,dealerhand,calculatescore(hand),calculatescore(dealerhand));
+
+                                    /// If userpoints goes over 21 then it's a bust - round is over for player.
+                                    if (calculatescore(hand)>21)
+                                        break;
+                                }
+
+                                /// If player wants to "DOUBLE" we double wager, add one card and break loop.
+                                else if (input=="D")
+                                {   
+                                    ///Validates if user has enough balance for this action
+                                    if ((wager*bothhands.Count()) + wager>bank)
+                                    {
+                                        Console.WriteLine("Insufficient bank balance");
+                                        continue;
+                                    }
+                                    wager *=2; ///// How to get back to wager if first hand was double and second was just a hit or stand
+                                    hand.Add(takecard());
+                                    showcards(hand,dealerhand,calculatescore(hand),calculatescore(dealerhand));
+                                    break;
+                                }
+                            }
+                            alluserhands.Add(hand);
+                        }
                         break;
                     }
-                    else
-                        break;
                 }
 
-                if (userpoints>21)
-                {
-                    bank -= wager;
-                    Console.WriteLine($"You went bust! You lost {wager}, current bank balance {bank}");
-                    continue;
-                }
-
+                /// Dealers turn to pick up cards, dealer has to HIT on <17 and STAY on >=17.
                 Console.WriteLine("\nDealers turn:");
-                while (dealerpoints<=16)
-                {
-                    dealerhand.Add(takecard().Item1);
-                    dealerpoints = calculatescore(dealerhand);
-                }
 
-                showcards(userhand,dealerhand,userpoints,dealerpoints);
+                while (calculatescore(dealerhand)<=16)
+                    dealerhand.Add(takecard());
 
-                if (dealerpoints>21)
+
+                /// Start to evaluate player hand(s) against dealer hand
+                foreach (List<string> hand in alluserhands)
+                { 
+                /// If player has more than 21, he has lost, take wager from bank, display results.
+                if (calculatescore(userhand)>21)
                 {
-                    bank += wager;
-                    Console.WriteLine($"Dealer went bust! You have won {wager}, current bank balance {bank}");
+                    bank -= wager;
+                    Console.WriteLine($"You went bust on hand {String.Join(" ", hand)}. You lost {wager}, current bank balance {bank}");
                     continue;
                 }
 
-                if (userpoints > dealerpoints)
-                { 
+                showcards(hand,dealerhand,calculatescore(hand),calculatescore(dealerhand));
+
+                /// If dealer went bust, player has won, add wager to bank and display result.
+                if (calculatescore(dealerhand)>21)
+                {
                     bank += wager;
-                    Console.WriteLine($"You won {wager}, current bank balance {bank}");
+                    Console.WriteLine($"Dealer went bust! You have won {wager} for {String.Join(" ", hand)} hand, current bank balance {bank}");
+                    continue;
                 }
-                else if (userpoints == dealerpoints)
-                { 
-                    Console.WriteLine($"It's a tie, current bank balance {bank}");
+
+                ///If both are in the game at this point compare results and make an impact on the bank
+                bank += getresults(calculatescore(hand), calculatescore(dealerhand), wager, bank);
+
+                /// If player is out of money, go to endgame and show results.
+                if (bank ==0)
+                    endgame(bank, startbank);
                 }
-                else
-                {
-                    bank -= wager;
-                    Console.WriteLine($"You lost {wager}, current bank balance {bank}");
-                }
-            if (bank ==0)
-                Console.WriteLine("Thank you for playing, you can't continue as you are out of money.");
             }
-        }
-        public static (string, int) takecard()
-        {
-            string[] cards = new string[] {"2","3","4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"};
-            var rndMember = cards[RandomNumberGenerator.GetInt32(cards.Length)];
-            int score = 0;
-            if (int.TryParse(rndMember, out int result))
-                score = int.Parse(rndMember);
-            else if (rndMember == "A")
-                score = 11;
-            else
-                score = 10;
-
-            return (rndMember, score);
-        }
-
-        public static void endgame(int bank)
-        {
-            Console.WriteLine("Thank you for playing.");
-                if (bank > 100)
-                    Console.WriteLine($"You have won {bank-100} dollars and walk away with {bank} dollars!");
-                else if (bank == 100)
-                    Console.WriteLine($"You have the same balance as you started with - {bank}!");
-                else
-                    Console.WriteLine($"You have lost {100-bank} dollars and walk away with {bank} dollars");
-            Console.ReadKey();
-            Environment.Exit(0);
-        }
-
-        public static void showcards(List<string> userhand, List<string> dealerhand, int userpoints, int dealerpoints)
-        {
-            Console.WriteLine($"You    :  {String.Join(" ", userhand)}  ({userpoints})");
-            Console.WriteLine($"Dealer :  {String.Join(" ", dealerhand)}  ({dealerpoints})");
-        }
-
-        public static int calculatescore(List<string> a)
-        {
-            int[] scores = new int[a.Count];
-            for (int i = 0; i < a.Count(); i++)
-            {
-                if (!int.TryParse(a[i], out int r))
-                {
-                    if (a[i] == "A")
-                        scores[i] = 11;
-                    else
-                        scores[i] = 10;
-                }
-                else
-                    scores[i] = int.Parse(a[i]);
-            }
-            while (scores.Sum() > 21 && Array.IndexOf(scores,11)!=-1)
-                scores[Array.IndexOf(scores,11)] = 1;
-
-            return scores.Sum();
         }
     }
 }

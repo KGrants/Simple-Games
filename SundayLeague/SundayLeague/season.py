865b7103dc0345ca06bbb9097173623182ed57c6
KGrants <118312276+KGrants@users.noreply.github.com>
2023-04-28 16:03:30 +0300

    yet another draft update for SundayLeague

--- SundayLeague/SundayLeague/season.py
+++ SundayLeague/SundayLeague/season.py
@@ -59,49 +59,49 @@ def season_screen():
 
 
 def game(home_team, away_team):
+    # calculating points for both teams based on 30% random chance and offence/defence ratio
     cur.execute("""SELECT Team, AVG(Offence), AVG(Defence)
                    FROM Players
                    WHERE 1 = 1 
                    AND Team in (%s, %s)
                    GROUP BY Team""" % (home_team, away_team))
     power = [i for i in cur.fetchall()]
-    coef = power[0][1]-power[1][1]+power[0][2]-power[1][2]
-    home_points = int(100*(1+(coef/100))*random.uniform(0.85,1.15))
-    away_points = int(100*(1-(coef/100))*random.uniform(0.85,1.15))
+    home_points = int(100*(1+(power[0][1]-power[1][2]/100))*random.uniform(0.85,1.15))
+    away_points = int(100*(1-(power[1][1]-power[0][2]/100))*random.uniform(0.85,1.15))
 
     # If there is a tie randomly add or remove one point from home team
     if home_points == away_points:
         home_points += 1 if random.random() < 0.5 else -1
+
+    # Declaring winner
     winner = home_team if home_points > away_points else away_team
 
+    # Inserting teams results in Games table
     cur.execute("""INSERT INTO Games 
                    (Home_Team, Away_Team, Home_Points, Away_Points, Winner)
                    VALUES
                    (%s, %s, %s, %s, %s)""" % (home_team, away_team, home_points, away_points, winner))
     conn.commit()
 
+    # Extracting game_id so that we can link player stats from Player_Score table
     cur.execute("""SELECT * 
                    FROM Games
                    ORDER BY 1 DESC""")
-
     game_id = cur.fetchone()[0]
 
+    # Generating individual points for all players and inserting them in Player_Score table.
     h_player_points = point_distribution(home_points,home_team)
     a_player_points = point_distribution(away_points,away_team)
 
-    home_ids = get_player_ids(home_team)
-    away_ids = get_player_ids(away_team)
-    
-    for i in range(5):
+    for key in h_player_points:
         cur.execute("""INSERT INTO Player_Score
                        VALUES
-                       (%s, %s, %s, %s)""" % (home_ids[i],home_team,game_id,h_player_points[i]))
+                       (%s, %s, %s, %s)""" % (key,home_team,game_id,h_player_points[key]))
 
-    for i in range(5):
+    for key in a_player_points:
         cur.execute("""INSERT INTO Player_Score
                        VALUES
-                       (%s, %s, %s, %s)""" % (away_ids[i],home_team,game_id,a_player_points[i]))
-
+                       (%s, %s, %s, %s)""" % (key,away_team,game_id,a_player_points[key]))
     return
 
 
@@ -112,18 +112,18 @@ def point_distribution(points, team_id):
                    AND Team = %s
                    ORDER BY Id""" % (team_id))
     ratings = [i[0] for i in cur.fetchall()]
+    rat_weight = points/sum(ratings)
     player_points = []
-    rat_sum = sum(ratings)
-    weight = points/rat_sum
-    for i in range(4):
-        player_points.append(int(weight*ratings[i]))
-    player_points.append(points-sum(player_points))
-    return player_points
 
-def get_player_ids(team_id):
+    for i in range(4):
+        player_points.append(int(rat_weight*ratings[i]))
+    player_points.append(points-sum(player_points))
+
     cur.execute("""SELECT id
                    FROM Players
                    WHERE 1 = 1
                    AND Team = %s
                    ORDER BY Id""" % (team_id))
-    return [i[0] for i in cur.fetchall()]
+    ids = [i[0] for i in cur.fetchall()]
+
+    return {ids[i]: player_points[i] for i in range(5)}
